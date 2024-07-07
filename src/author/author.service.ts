520ebe00c0d8741ff3d83d1a55c0bd7cae4d02aa
ShotaLaabb <shotikoslabadzes1@gmail.com>
2024-07-07 14:10:13 +0400

    updated code

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -2,32 +2,32 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { AuthorRepository } from './author.repository';
+<<<<<<< Updated upstream
 import { AuthorEntity } from './entities/author.entity';
+=======
+>>>>>>> Stashed changes
 
 @Injectable()
 export class AuthorService {
-  searchAuthors(search: string): AuthorEntity[] {
-    throw new Error('Method not implemented.');
-  }
-  constructor(private readonly authorsRepository: AuthorRepository) {}
+  constructor(private readonly authorRepository: AuthorRepository) {}
 
   create(createAuthorDto: CreateAuthorDto) {
-    return this.authorsRepository.create(createAuthorDto);
+    return this.authorRepository.create(createAuthorDto);
   }
 
   findAll() {
-    return this.authorsRepository.findAll();
+    return this.authorRepository.findAll();
   }
 
   findOne(id: number) {
-    return this.authorsRepository.findOne(id);
+    return this.authorRepository.findOne(id);
   }
 
   update(id: number, updateAuthorDto: UpdateAuthorDto) {
-    return this.authorsRepository.update(id, updateAuthorDto);
+    return this.authorRepository.update(id, updateAuthorDto);
   }
 
   remove(id: number) {
-    return this.authorsRepository.remove(id);
+    return this.authorRepository.remove(id);
   }
 }
