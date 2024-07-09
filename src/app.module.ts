b699b8e06a7406987dbef497e5e98d2aebe02aee
ShotaLaabb <shotikoslabadzes1@gmail.com>
2024-07-09 16:25:51 +0400

    make some fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { AlbumModule } from './album/album.module';
-import { MusicModule } from './music/music.module';
-import { SearchModule } from './search/search.module';
-import { AuthorModule } from './author/author.module';
-
-@Module({
-  imports: [AuthorModule, MusicModule, SearchModule],
-=======
 import { MusicModule } from './music/music.module';
 import { SearchModule } from './search/search.module';
 import { AuthorModule } from './author/author.module';
@@ -17,7 +8,6 @@ import { AlbumModule } from './album/album.module';
 
 @Module({
   imports: [MusicModule, SearchModule, AuthorModule, AlbumModule],
->>>>>>> feat/INTS-32/Implement-Search-Module
   controllers: [AppController],
   providers: [AppService],
 })
