ddb3b8d0b94da7f8f185d293e86c5d1494e52520
ShotaLaabb <52139226+ShotikoLabadze@users.noreply.github.com>
2024-09-24 18:07:43 +0400

    exclude admins in table

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -22,6 +22,7 @@ export class UserRepository {
 
     const newUser = this.userRepository.create({
       ...createUserDto,
+      isAdmin: false,
       password: await bcrypt.hash(createUserDto.password, 10),
     });
 
@@ -46,7 +47,7 @@ export class UserRepository {
 
   async findAll(): Promise<UserEntity[]> {
     return await this.userRepository
-      .createQueryBuilder('user')
+      .createQueryBuilder('user').where('user.isAdmin = :isAdmin', { isAdmin: false })
       .orderBy('user.createdAt', 'DESC')
       .leftJoinAndSelect('user.playlists', 'playlist')
       .getMany();
